c5b1dbbf337c77d5059bb583f651619d3a9c810f
khaldoun36 <246844121+khaldoon-dev@users.noreply.github.com>
2026-04-29 15:29:01 +0400

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,38 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
+import icon from "astro-icon";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [icon()],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // Injects this line into every <style lang="scss"> block
+          additionalData: `@use "/src/styles/_utilities.scss" as *;`,
+        },
+      },
+    },
+  },
+
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Mona Sans",
+      cssVariable: "--font-mona",
+      options: {
+        variants: [
+          {
+            weight: "300 900",
+            style: "normal",
+            src: ["./src/assets/fonts/MonaSansVF[wght,opsz,ital].woff2"],
+          },
+          {
+            weight: "300 900",
+            style: "italic",
+            src: ["./src/assets/fonts/MonaSansVF[wght,opsz,ital].woff2"],
+          },
+        ],
+      },
+    },
+  ],
+});
